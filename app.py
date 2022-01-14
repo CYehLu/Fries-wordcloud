0df12bc83734be827895ace40a34bb8f06f718a6
ChunYehLu <window120909120909@gmail.com>
2022-01-14 17:04:21 +0800

    change folder structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, send_file, redirect, request
-from tools import plot_wordcloud_by_userid
+from core.tools import plot_wordcloud_by_userid
 
 app = Flask(__name__)
 
@@ -11,11 +11,11 @@ def home():
             userid = request.values['userid']
             
             if check_picture_pool(userid):
-                return send_file(f'wordclouds/{userid}_filtered.png')
+                return send_file(f'core/wordclouds/{userid}_filtered.png')
             else:
                 try:
                     plot_wordcloud_by_userid(userid)
-                    return send_file(f'wordclouds/{userid}_filtered.png')
+                    return send_file(f'core/wordclouds/{userid}_filtered.png')
                 except ValueError:
                     return render_template('error.html')
             
@@ -25,7 +25,7 @@ def check_picture_pool(userid):
     """
     Check if {userid}.png has already existed at wordclouds/
     """
-    all_pngs = os.listdir('wordclouds/')
+    all_pngs = os.listdir('core/wordclouds/')
     return any(filter(lambda s: s == f'{userid}.png', all_pngs))
     
 if __name__ == '__main__':
